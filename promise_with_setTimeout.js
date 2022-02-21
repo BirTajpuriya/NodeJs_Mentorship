c21ee0edf53781c56fcca6b4e340a7c8403f5aec
BirTajpuriya <tajpuriyabir00@gmail.com>
2022-02-21 20:38:48 +0545

    little bit chaange

--- promise_with_setTimeout.js
+++ promise_with_setTimeout.js
@@ -1,11 +1,9 @@
-
 // Code start here
 // function newPromise(){
 //     return new Promise((res,rej)=>{
 //         setTimeout(()=>{
 //             res("success");
 //         },2000)
-        
 
 //     })
 // }
@@ -14,11 +12,9 @@
 //     console.log(data);
 // })
 
-// code ends 
-
+// code ends
 
 //code start here
-
 
 //code start here
 // async function  doInput(){
@@ -37,18 +33,17 @@
 
 //code ends here
 
-function display(){
-    return 3;
-    // console.log('this is display function');
+function display() {
+  return 3;
+  // console.log('this is display function');
 }
 
-console.log('hello');
-async function hello(){
-console.log('this is node js class');
-let item=await display();
-console.log(item);
+console.log("hello");
+
+async function hello() {
+  console.log("this is node js class");
+  let item = await display();
+  console.log(item);
 }
 hello();
-console.log('this is outside the block');
-
-
+console.log("this is outside the block");
