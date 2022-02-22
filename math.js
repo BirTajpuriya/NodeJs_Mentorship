092f0bae6030bbe2bc0fd0ff439c8b44ae0e8fab
BirTajpuriya <tajpuriyabir00@gmail.com>
2022-02-22 13:49:40 +0545

    math function

---
+++ math.js
@@ -0,0 +1,27 @@
+// jati pani math ko function use garnu Xa,
+// tyo function ko agadi Math Keyword ko use garnu parxa
+
+let a= Math.round(2.4);
+console.log(a);// output: 2
+
+//round le .5 bhanda matthi ko value lekhe, roundoff garer value display garxa
+
+// trunc le decimal paxiko sabai value erase gardinxa
+let b=Math.trunc(8.19);
+console.log(b);// output:8
+
+
+let res=Math.min(1,2,3,4);
+console.log(res);// output: 1
+
+let squreRoot=Math.sqrt(81);// find the square root
+console.log(squreRoot);
+
+let cube=Math.cbrt(125);//this is for cube root
+console.log(cube);
+
+let findPowerOf=Math.pow(5,3);// 5*5*5
+console.log(findPowerOf);// output: 125
+
+let unknownNum=Math.random();
+console.log(unknownNum);// each time random number will generate
